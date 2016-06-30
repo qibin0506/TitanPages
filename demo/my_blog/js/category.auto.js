71a35684b9d51f9bfdef08102536aee07b300b93
qibin <qibin0506@gmail.com>
2016-06-30 23:33:35 +0800

    upload project

---
+++ demo/my_blog/js/category.auto.js
@@ -0,0 +1,27 @@
+
+var defaultPageSize = 5
+var arr = eval('[{"title":"我的第一篇博客 (另一个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第10个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第11个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第12个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第13个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第14个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第15个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第16个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第17个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第18个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第19个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第20个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第21个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第22个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第23个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第24个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第25个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第26个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第27个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第28个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第29个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第3个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第4个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第5个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第6个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第7个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第8个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客 (第9个复件)","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"},{"title":"我的第一篇博客","date":"2016-06-30 23:22:39","desc":"这是我的第一篇博客, 主要是用来做测试的, `TitanPages`是不是可以完美的生成静态博客呢?"}]')
+function get(currentPage) {
+	return getResult(currentPage, defaultPageSize);
+}
+function getResult(currentPage, pageSize) {
+	currentPage = parseInt(currentPage);
+	pageSize = parseInt(pageSize);
+	var startIndex = (currentPage - 1) * pageSize;
+	var endIndex = startIndex + pageSize;
+	if (arr.length <= startIndex) { return null;}
+	if (endIndex > arr.length) { endIndex = arr.length;}
+	return arr.slice(startIndex, endIndex);
+}
+function pageCount() {
+	return getPageCount(defaultPageSize);
+}
+function getPageCount(pageSize) {
+	return Math.ceil(arr.length / pageSize);
+}
+function getQueryString(query) {
+	var uri = window.location.search;
+    var re = new RegExp("" +query+ "=([^&?]*)", "ig");
+    return ((uri.match(re))?(uri.match(re)[0].substr(query.length+1)):null);
+}
+	
---
+++ source/tt.go
@@ -0,0 +1,317 @@
+package main
+
+//write by qibin(http://blog.csdn.net/qibin0506)
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/russross/blackfriday"
+	"github.com/toqueteos/webbrowser"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"text/template"
+	"time"
+)
+
+const (
+	create = "create" // create new file
+	view   = "view"   // view as html
+	build  = "build"  // convert markdown file to html file
+	cate   = "cate"   // create category
+
+	defaultContent = `### hello markdown`
+
+	categoryJs = `
+var defaultPageSize = 5
+var arr = eval('{{.}}')
+function get(currentPage) {
+	return getResult(currentPage, defaultPageSize);
+}
+function getResult(currentPage, pageSize) {
+	currentPage = parseInt(currentPage);
+	pageSize = parseInt(pageSize);
+	var startIndex = (currentPage - 1) * pageSize;
+	var endIndex = startIndex + pageSize;
+	if (arr.length <= startIndex) { return null;}
+	if (endIndex > arr.length) { endIndex = arr.length;}
+	return arr.slice(startIndex, endIndex);
+}
+function pageCount() {
+	return getPageCount(defaultPageSize);
+}
+function getPageCount(pageSize) {
+	return Math.ceil(arr.length / pageSize);
+}
+function getQueryString(query) {
+	var uri = window.location.search;
+    var re = new RegExp("" +query+ "=([^&?]*)", "ig");
+    return ((uri.match(re))?(uri.match(re)[0].substr(query.length+1)):null);
+}
+	`
+)
+
+// md -type create -file my
+// md -type view -file my
+// md -type build -file my (-tmpl template.html -author loader -datefmt 2016-06-28)
+var tp *string = flag.String("type", create, "")
+var fileName *string = flag.String("file", "default", "")
+var tmpl *string = flag.String("tmpl", "", "")
+var author *string = flag.String("author", "", "")
+
+var mdDir string = "./raw/"
+var htmlDir string = "./html/"
+var dateFmt string = "2006-01-02 15:04:05"
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	checkType(*tp)
+	checkDir()
+
+	switch *tp {
+	case view:
+		viewContent(*fileName)
+	case create:
+		createContent(*fileName)
+	case build:
+		buildContent(*fileName)
+	case cate:
+		buildCategory()
+	}
+}
+
+// 生成目录js
+func buildCategory() {
+	jsFile := htmlDir + "category.auto.js"
+	deleteOldFile(jsFile)
+
+	dirs, err := ioutil.ReadDir(htmlDir)
+	checkError(err)
+
+	var cates []*Category
+	for _, item := range dirs {
+		title := item.Name()
+		if strings.HasSuffix(title, ".html") {
+			title = strings.TrimRight(title, ".html")
+		} else if strings.HasSuffix(title, ".htm") {
+			title = strings.TrimRight(title, ".htm")
+		}
+
+		cate := &Category{Title: title, Date: item.ModTime().Format(dateFmt), Desc: getContentDesc(item)}
+		cates = append(cates, cate)
+	}
+
+	jsonData, err := json.Marshal(cates)
+	checkError(err)
+
+	outFile, err := os.Create(jsFile)
+	checkError(err)
+	defer outFile.Close()
+	t := template.Must(template.New("category.auto.js").Parse(categoryJs))
+	t.Execute(outFile, string(jsonData))
+	fmt.Println("category data '" + jsFile + "' build success! now you can use it in your html file")
+}
+
+// 生成html内容文件
+func buildContent(name string) {
+	mdFile := mdDir + name + ".md"
+	if !fileExist(mdFile) {
+		createContent(name)
+		return
+	}
+
+	f, err := os.Open(mdFile)
+	checkError(err)
+	defer f.Close()
+	fd, err := ioutil.ReadAll(f)
+	checkError(err)
+
+	html := blackfriday.MarkdownCommon(fd)
+	art := &Article{Title: name, Date: time.Now().Format(dateFmt), Author: *author, Content: string(html), Desc: parseContentDesc(fd)}
+	convertHtml(name, html, art)
+}
+
+// 查看html文件
+func viewContent(name string) {
+	file := htmlDir + name + ".html"
+	if !fileExist(file) {
+		if fileExist(mdDir + name + ".md") {
+			buildContent(name)
+		} else {
+			createContent(name)
+		}
+
+		return
+	}
+
+	fmt.Println("wait a moment...")
+	time.AfterFunc(time.Duration(1)*time.Second, func() {
+		webbrowser.Open("http://127.0.0.1:8080/" + name + ".html")
+	})
+	HandleHttp()
+}
+
+// 创建markdown文件
+func createContent(name string) {
+	file := mdDir + name + ".md"
+	deleteOldFile(file)
+
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	mdFile, err := os.Create(file)
+	checkError(err)
+
+	defer mdFile.Close()
+	_, err = mdFile.WriteString(defaultContent)
+	checkError(err)
+
+	fmt.Println(file + " create successed! now you can edit it!!!")
+}
+
+func HandleHttp() {
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(htmlDir))))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// 从html中提取description
+func getContentDesc(fileInfo os.FileInfo) (desc string) {
+	name := fileInfo.Name()
+	info, err := ioutil.ReadFile(htmlDir + name)
+	checkError(err)
+	content := string(info)
+	re := regexp.MustCompile(`(?i)<meta.*?name=["|\']description["|\'].*?content=["|\'](?P<desc>.*?)["|\']`)
+	res := re.FindAllStringSubmatch(content, -1)
+	if res != nil && len(res) > 0 {
+		desc = res[0][len(res[0])-1]
+	}
+	return
+}
+
+// 从markdown中提取一行作为description
+func parseContentDesc(content []byte) (desc string) {
+	buf := bufio.NewScanner(strings.NewReader(string(content)))
+	for buf.Scan() {
+		line := buf.Text()
+		if strings.HasPrefix(line, "#") || line == "" {
+			continue
+		}
+		desc = line
+		break
+	}
+	return
+}
+
+// 将markdown内容转化为html，并写入模板中
+func convertHtml(name string, html []byte, content interface{}) {
+	htmlFile := htmlDir + name + ".html"
+	deleteOldFile(htmlFile)
+
+	if *tmpl == "" {
+		// raw
+		err := ioutil.WriteFile(htmlFile, html, 0777)
+		checkError(err)
+	} else {
+		// template
+		outFile, err := os.Create(htmlFile)
+		checkError(err)
+		defer outFile.Close()
+		t := template.Must(template.ParseFiles(*tmpl))
+		err = t.Execute(outFile, content)
+		checkError(err)
+	}
+
+	var arg string
+	scan(&arg, "build '"+htmlFile+"' success!!!\nview it now? (Y/N)")
+
+	switch arg {
+	case "y":
+		fallthrough
+	case "Y":
+		viewContent(name)
+	}
+}
+
+// 删除旧文件
+func deleteOldFile(file string) {
+	if !fileExist(file) {
+		return
+	}
+
+	var arg string
+	scan(&arg, "old file '"+file+"' exist!!! \n do you want to create a new one? (Y/N)")
+
+	switch arg {
+	case "n":
+		fallthrough
+	case "N":
+		log.Fatal("exit by user")
+	case "y":
+		fallthrough
+	case "Y":
+		if err := os.Remove(file); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func scan(arg *string, notice string) {
+	fmt.Print(notice)
+	fmt.Scanln(arg)
+}
+
+func fileExist(file string) bool {
+	_, err := os.Stat(file)
+	if err == nil {
+		return true
+	}
+
+	return os.IsExist(err)
+}
+
+func checkDir() {
+	err := os.MkdirAll(htmlDir, 0777)
+	checkError(err)
+	err = os.MkdirAll(mdDir, 0777)
+	checkError(err)
+}
+
+func checkType(tp string) {
+	if tp != create && tp != view && tp != build && tp != cate {
+		log.Fatal("type" + tp + " is unavailable")
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// 文章
+type Article struct {
+	Title   string
+	Date    string
+	Author  string
+	Content string
+	Desc    string
+}
+
+// 目录
+type Category struct {
+	Title string `json:"title"`
+	Date  string `json:"date"`
+	Desc  string `json:"desc"`
+}
